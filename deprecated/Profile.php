a46119f64a54e1d6781e0a9357fb1ae2c0f792e0
alimsihui <71798618+alimsihui@users.noreply.github.com>
2020-11-09 10:53:04 +0800

    Profile Moved

--- deprecated/Profile.php
+++ deprecated/Profile.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
-<?php
-require_once "../model/protect.php";
-session_start();
-?>
+
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -44,21 +41,6 @@ session_start();
           background-color:#34558b;
         }
     </style>
-    
-    <!-- Vue JS -->
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-
-    <!--Axios-->
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-
-    <!--Bootstrap JQUERY and Popper-->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-
-
-    <!--Bootstrap-->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-
 </head>
 
 <body>
@@ -129,7 +111,7 @@ session_start();
       </nav>
     </div>
 
-    <div class = "container" id = "app">
+    <div class = "container">
 
 
         <div class = "row">
@@ -147,68 +129,126 @@ session_start();
             </div>
 
             <div class="col-lg-8 my-3">
+              
                 <div class="card card-fluid">
                   <h6 class="card-header"> Public Profile </h6>
-                    <div class="card-body">
+                  <div class="card-body">
 
-                        <div>
-                            <h3>Personal Details</h3>
+                    <div class="media mb-3">
+                      <div class="user-avatar user-avatar-xl fileinput-button">
+                        <div class="fileinput-button-label"> Change photo </div>
+                        <img src="assets/images/avatars/profile.jpg" alt="User Avatar">
+                        <input id="fileupload-avatar" type="file" name="avatar"> </div>
 
-                            <h4>Your username is: {{username}}</h4>
+                      <div class="media-body pl-3">
+                        <h3 class="card-title"> Public avatar </h3>
+                        <h6 class="card-subtitle text-muted"> Click the current avatar to change your photo. </h6>
+                        <p class="card-text">
+                          <small>JPG, GIF or PNG 400x400, &lt; 2 MB.</small>
+                        </p>
 
-                            <h4>Your fullname is: {{fullname}}</h4>
-
-                            <h4>Your email is: {{email}}</h4>
-                        
+                        <div id="progress-avatar" class="progress progress-xs fade">
+                          <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
+                        
+                      </div>
+                      
                     </div>
-                    <a href="#chart" style="margin-bottom: 20px;" class="btn btn-info" data-toggle="collapse">View Personal Earnings</a>
-                <div class="bg-light collapse " style="width: 70%;margin-left:30px;"  id="chart">
+                    <a href="#chart" style="margin-bottom: 20px;" class="btn btn-danger" data-toggle="collapse">View Personal Earnings</a>
+  
+  <div class="bg-light collapse " style="width: 70%;margin-left:30px;"  id="chart">
     <span style="font-size: 30px;color: black;
     font-weight: 700;" id="total"></span>
     <canvas id="myChart" ></canvas></div>
-                </div>
+  </div>
+                    <div class = "row">
+
+                        <div class = "col-md-3 mb-3">
+                          
+                            Rating as a Hero
+                        </div>
+
+                        <div class = "col-md-9 mb-3">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </div>
+
+                    </div>
+
+                    <div class = "row">
+
+                        <div class = "col-md-3 mb-3">
+                            Rating as a Client
+                        </div>
+
+                        <div class = "col-md-9 mb-3">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </div>
+
+                    </div>
+
+                    
+                    
+                    <form method="post">
+                      
+                      <div class="form-row">
+                      
+                        <label for="input01" class="col-md-3">Profile Heading</label>
+                        
+                        
+                        <div class="col-md-9 mb-3">
+                          <textarea type="text" class="form-control" id="input01"></textarea>
+                          <small class="text-muted">Appears on your profile page, 300 chars max.</small>
+                        </div>
+                    
+                      </div>
                 
-            </div>
-            
-            
+                      
+                      <div class="form-row">
+                    
+                        <label for="input02" class="col-md-3">Available for hire?</label>
+                    
+                        <div class="col-md-9 mb-3">
+                          <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="input02" checked>
+                            <label class="custom-control-label" for="input02">Yes, hire me</label>
+                          </div>
+                          <small class="text-muted">You will receive notifications for jobs.</small>
+                        </div>
+                      
+                      </div>
+                      
+                      <hr>
+                      
+                      <div class="form-actions">
+                        <button type="submit" class="btn btn-primary ml-auto">Update Profile</button>
+                      </div>
+                      
+                    </form>
+                    
+                  </div>
+                </div>
+
 
         </div>
-    </div>
-<script>
-
-const vm = new Vue({
-    el: '#app',
-    data: {
-        test: "hello",
-        username: "",
-        fullname: "",
-        email: "",
-
-    },
-    methods: {
-        getUserDetails: function() {
-                axios.get('../Main/getUserDetails.php')
-                .then(response => {
-                    this.username = response.data.username;
-                    this.fullname = response.data.fullname;
-                    this.email = response.data.email;
-                })
-                .catch(error => console.log('Could not retrieve user details...'));
-        }
-    },
-    mounted: function(){
-        this.getUserDetails();
-    }
-});
-
-</script>
+        
+ 
+  </div>
+    
 
 
-<!--Chart-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.bundle.js" ></script>
-<script src="profile.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    
 
 </body>
+<script src="profile.js"></script>
 </html>
