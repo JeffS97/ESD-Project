a92be5b351e56688d6aa4c007b39d3f347a1e4ba
Jeffvinder Singh <40801372+JeffS97@users.noreply.github.com>
2020-11-12 16:58:57 +0800

    Update process_taskview.php

--- Main/process_taskview.php
+++ Main/process_taskview.php
@@ -242,7 +242,7 @@
                 <span><a class="btn btn-warning ml-5 mb-2" href="../views/Chat.php?id=<?php echo $gigArray[0]->getId()?>">Chat with Hero</a></span>
                 </div>
                 <div id="map"></div>
-                <div id="duration">Unable to compute travel time and route due to incorrect address format. <br> Rest assured! your hero is still on his way</div>
+                <div id="duration"><?php if($gigArray[0]->getAccepteraddress()===null){echo "No Hero has come to your rescue yet! Please be patient";}else{echo "Unable to compute travel time and route due to incorrect address format. <br> Rest assured! your hero is still on his way";}?></div>
             </div>
         </div>
     </body>
