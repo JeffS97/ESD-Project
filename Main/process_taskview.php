74f2235db42726d5a98282e4585bb5664cc485f7
Jeffvinder Singh <40801372+JeffS97@users.noreply.github.com>
2020-11-12 11:49:47 +0800

    Update process_taskview.php

--- Main/process_taskview.php
+++ Main/process_taskview.php
@@ -214,33 +214,34 @@
               <li>  <button type='button'  class='btn btn-info'  ><a href='./process_logout.php' style='color: white;text-decoration: none;'>Log Out</a></button></li>
                 <?php }else{?>
                     
-               
-                <!-- <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="background-color: transparent;">
-                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-                </form> -->
                 
          <li> <button type="button"  class="btn btn-info" ><a href="../views/Signup.php" style="color: white;text-decoration: none;">Join Us</a></button></li>  
                 <?php }?>  
-                       <!-- <button type="button" class="btn btn-primary" style="margin: 10px;">Sign Up</button> -->
                 <span class = 'noti' style="padding: 10px; font-size: 25px; padding-bottom: 15px;" hidden><img src = "https://www.flaticon.com/svg/static/icons/svg/523/523152.svg" height = 35px width = 35px> </span>
             </div>
          </ul>  
         </div>
         </nav>
-
-        <body>
-            <div id="floating-panel">
-            <b>Your Address: </b>
-            <span id="start"><?php echo  $gigArray[0]->getBookeraddress()?>
-            </span>
-            <b>Hero's Location: </b>
-            <span id="end"><?php echo  $gigArray[0]->getAccepteraddress()?>
-            </span>
+        <div class="container">
+            <div class="row">
+                <div id="floating-panel">
+                <b>Your Address: </b>
+                <span id="start"><?php echo  $gigArray[0]->getBookeraddress()?>
+                </span>
+                <b>Hero's Location: </b>
+                <span id="end"><?php echo  $gigArray[0]->getAccepteraddress()?>
+                </span>
+                </div>
+                <div id="map"></div>
+                <div id="duration">Unable to compute travel time and route due to incorrect address format. <br> Rest assured! your hero is still on his way</div>
             </div>
-            <div id="map"></div>
-            <div id="duration">Unable to compute travel time and route</div>
-        </body>
+        </div>
+    </body>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
 
         <script>
             function initMap() {
