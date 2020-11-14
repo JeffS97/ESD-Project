47337ef33eec9f05929e052e9b35aab3d686fd08
rohan <rohanmanojk.2019@sis.smu.edu.sg>
2020-11-14 12:48:56 +0800

    send

--- Main/process_login.php
+++ Main/process_login.php
@@ -6,8 +6,6 @@
         }
     );
     session_start();
-
-    # Get parameters passed from login.php
     $email = $_POST["email"];
     $password = $_POST["password"];
 
@@ -17,34 +15,58 @@
 
     # Check if user exists 
     $success = false;
-    if($user != null){
-        # Get stored hashed password
-        $hashed = $user->getPassword();
-        $fullname = $user->getFullName();
-        $username = $user->getFullName();
-       
-        # Check if entered password matches stored hashed password
-        $success = password_verify($password,$hashed); 
-        var_dump(password_hash($password,PASSWORD_DEFAULT));
-        var_dump($hashed);
-        if($success){
-            # Create a session entry for successful login
-            $_SESSION["email"] = $email;
-            $_SESSION["fullname"] = $fullname;
-            $_SESSION["username"] = $username;
-            # Redirect to welcome.php
-            header("Location: ../Homepage.php");
-            exit;
-        }
-    }
-
-    # Failed login
-    if (!$success){
-        # Add "error" => "Failed Login" key-value pair to the session
-        $_SESSION["error"] = "Failed Login";
-        # Redirect to login.php, while passing username information 
-        # at the back of the URL, e.g., header("Location: login.php?...");
-       header("Location: ../views/Signup.html");
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))
+    {
+          $secret = '6Lf2x-IZAAAAAKgiSKXLf0p4aHiSTjdxEQGJAQM8';
+          $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+          $responseData = json_decode($verifyResponse);
+          if($responseData->success)
+          {
+            if($user != null){
+                # Get stored hashed password
+                $hashed = $user->getPassword();
+                $fullname = $user->getFullName();
+                $username = $user->getFullName();
+               
+                # Check if entered password matches stored hashed password
+                $success = password_verify($password,$hashed); 
+                var_dump(password_hash($password,PASSWORD_DEFAULT));
+                var_dump($hashed);
+                if($success){
+                    # Create a session entry for successful login
+                    $_SESSION["email"] = $email;
+                    $_SESSION["fullname"] = $fullname;
+                    $_SESSION["username"] = $username;
+                    # Redirect to welcome.php
+                    header("Location: ../Homepage.php");
+                    exit;
+                }
+            }
+        
+            # Failed login
+            if (!$success){
+                # Add "error" => "Failed Login" key-value pair to the session
+                $_SESSION["error"] = "Failed Login";
+                # Redirect to login.php, while passing username information 
+                # at the back of the URL, e.g., header("Location: login.php?...");
+               header("Location: ../views/Signup.php");
+               $_SESSION["error"]="Wrong Credentials";
+                exit; 
+            }
+          }
+          else
+          {
+            header("Location: ../views/Signup.php");
+            $_SESSION["error"]="Wrong Credentials";
+            exit; 
+          }
+     }
+     else{
+        header("Location: ../views/Signup.php");
+        $_SESSION["error"]="Must verify the reCaptcha";
         exit; 
-    }
+     }
+    # Get parameters passed from login.php
+   
+   
 ?>
